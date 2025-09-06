08bb7052205b5f7c5d86e876d2d9137b4412f891
Samuel Bonifacio <131225632+samuelbonifacio015@users.noreply.github.com>
2025-09-06 14:59:36 -0500

    updated opp-sample #2
    (added ProductId in /sales)

--- Semana 2/Sabado 6-09/opp-sample/src/main/java/acme/opp/sales/ProductId.java
+++ Semana 2/Sabado 6-09/opp-sample/src/main/java/acme/opp/sales/ProductId.java
@@ -1,4 +1,32 @@
 package acme.opp.sales;
 
-public record ProductId() {
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Reoresents a unique Identifier for a product within Sales bounded context.
+ * @param value the product identifier value, it must not be null for sure.
+ * @author Samuel Bonifacio (Open Source Application Development Team)
+ */
+
+public record ProductId(UUID value) {
+
+    /**
+     * COnstructs a ProductId object with validation.
+     * @param value the product identifier value.
+     * @throws IllegalArgumentException if the value is null.
+     */
+    public ProductId {
+        if (Objects.isNull(value)) {
+            throw new IllegalArgumentException("Product id cannot be null");
+        }
+    }
+
+    /**
+     * Constructs a ProductId with a random UUID value.
+     */
+
+    public ProductId() {
+        this (UUID.randomUUID());
+    }
 }
