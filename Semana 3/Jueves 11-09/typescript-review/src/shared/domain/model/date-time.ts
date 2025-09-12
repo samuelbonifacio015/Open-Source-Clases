41fb8b680b211a106f58faa72f6145d5d014edc4
Samuel Bonifacio <131225632+samuelbonifacio015@users.noreply.github.com>
2025-09-11 22:01:52 -0500

    added typescript test (jueves)

---
+++ Semana 3/Jueves 11-09/typescript-review/src/shared/domain/model/date-time.ts
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+
+export class DateTime {
+    private readonly _date: Date;
+
+    /**
+     * Creates a new DateTime instance.
+     * @remarks
+     * If no value is provided, it defaults to the current date and time.
+     * Throws an error if the provided date is invalid or in the future.
+     * @param value
+     */
+
+    constructor(value? : Date | string) {
+        const now = new Date();
+        if (value) {
+            const parsedDate = new Date(value);
+            if (isNaN(parsedDate.getTime())) throw Error (`Invalid date: ${parsedDate}`);
+            if (parsedDate > now) throw Error(`Date cannot be in the future: ${parsedDate}`);
+            this._date = parsedDate;
+        }else
+            this._date = now;
+    }
+
+    /**
+     * Gets the underlying Date object.
+     * @returns {Date} the Date object.
+     */
+
+    public get value(): Date {
+        return this._date;
+    }
+
+    /**
+     * Formats the date to a human-readable string in the specified locale.
+     * Defaults to 'en-US' if no locale is provided.
+     * @returns {string} the formatted date string.
+     * @param locale
+     */
+
+    public format (locale: string = 'en-US') : string {
+        return this._date.toLocaleDateString(locale,
+            {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            });
+    }
+
+    /**
+     * Returns the ISO 8601 string representation of the date.
+     * @returns {string} the ISO 8601 formatted date string.
+     */
+    public toString(): string {
+        return this._date.toISOString();
+    }
+}
