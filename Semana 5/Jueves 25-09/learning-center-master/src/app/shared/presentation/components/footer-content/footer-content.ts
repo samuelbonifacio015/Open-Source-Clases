5d482dfc650ba121ea7615e643b4e7ed4b367d78
Samuel Bonifacio <131225632+samuelbonifacio015@users.noreply.github.com>
2025-09-25 20:25:24 -0500

    initial setup for learning center master

---
+++ Semana 5/Jueves 25-09/learning-center-master/src/app/shared/presentation/components/footer-content/footer-content.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-content',
+  imports: [],
+  templateUrl: './footer-content.html',
+  styleUrl: './footer-content.css'
+})
+export class FooterContent {
+
+}
---
+++ Semana 5/Jueves 25-09/learning-center-master/src/app/shared/presentation/components/language-switcher/language-switcher.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-switcher',
+  imports: [],
+  templateUrl: './language-switcher.html',
+  styleUrl: './language-switcher.css'
+})
+export class LanguageSwitcher {
+
+}
