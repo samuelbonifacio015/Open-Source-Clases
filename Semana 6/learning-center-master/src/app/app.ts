cfc42b09878a7544467856190aae8b05eb1d67ee
Samuel Bonifacio <131225632+samuelbonifacio015@users.noreply.github.com>
2025-10-22 15:02:57 -0500

    learning-center final version update

---
+++ Semana 6/learning-center-master/src/app/app.ts
@@ -0,0 +1,20 @@
+import {Component, inject, signal} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {Layout} from './shared/presentation/components/layout/layout';
+
+@Component({
+  selector: 'app-root',
+  imports: [Layout],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('learning-center');
+  private translate: TranslateService;
+
+  constructor() {
+    this.translate = inject(TranslateService);
+    this.translate.addLangs(['en', 'es']);
+    this.translate.use('en');
+  }
+}
